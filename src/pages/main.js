241e44fcb140e0ee0a2551780524d679dcec9737
Ayomide Fayomi <storario2@gmail.com>
2021-12-04 07:59:43 +0100

    main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,20 +1,19 @@
-
-import Hero from '../components/hero.js/hero';
-import TopListing from '../components/topListing/topListing';
-import Store from '../components/store/store';
-import Form from '../components/form/Form';
-import Footer from '../components/footer/Footer'
+import Hero from "../components/hero.js/hero";
+import TopListing from "../components/topListing/topListing";
+import Store from "../components/store/store";
+import Form from "../components/form/Form";
+import Footer from "../components/footer/Footer";
 
 const main = () => {
-    return (
-        <div>
-        <Hero />
-        <TopListing />
-        <Store />
-        <Form />
-        <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Hero />
+      <TopListing />
+      <Store />
+      <Form />
+      <Footer />
+    </div>
+  );
+};
 
-export default main
+export default main;
